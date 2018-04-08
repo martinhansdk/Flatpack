86c372f4bcbc4993fc4c24836b8b4463b4dafd94
Martin Hans <martinhansdk@gmail.com>
2018-04-08 10:01:54 -0700

    Added code for iterating over selected face geometry.

--- Flatpack.cpp
+++ Flatpack.cpp
@@ -5,6 +5,7 @@
 #include <Core/Application/ValueInput.h>
 #include <Core/Application/UnitsManager.h>
 #include <Core/Application/ObjectCollection.h>
+#include <Core/CoreTypeDefs.h>
 #include <Core/UserInterface/UserInterface.h>
 #include <Core/UserInterface/CommandCreatedEventHandler.h>
 #include <Core/UserInterface/CommandCreatedEvent.h>
@@ -19,19 +20,28 @@
 #include <Core/UserInterface/ValueCommandInput.h>
 #include <Core/UserInterface/StringValueCommandInput.h>
 #include <Core/UserInterface/SelectionCommandInput.h>
+#include <Core/UserInterface/Selection.h>
 #include <Core/UserInterface/ToolbarPanel.h>
 #include <Core/UserInterface/ToolbarPanelList.h>
 #include <Core/UserInterface/ToolbarControl.h>
 #include <Core/UserInterface/ToolbarControls.h>
 #include <Core/UserInterface/ListItems.h>
-#include <Core/Geometry/Point3D.h>
-#include <Core/Geometry/Vector3D.h>
+#include <Core/Geometry/Curve2D.h>
+#include <Core/Geometry/NurbsCurve2D.h>
+#include <Core/Geometry/Point2D.h>
 #include <Core/UserInterface/DirectionCommandInput.h>
 #include <Core/UserInterface/DistanceValueCommandInput.h>
 #include <Core/UserInterface/TableCommandInput.h>
 #include <Core/UserInterface/AngleValueCommandInput.h>
 
+#include <Fusion/BRep/BRepCoEdge.h>
+#include <Fusion/BRep/BRepCoEdges.h>
+#include <Fusion/BRep/BRepFace.h>
+#include <Fusion/BRep/BRepLoop.h>
+#include <Fusion/BRep/BRepLoops.h>
+
 #include <sstream>
+#include <string>
 
 using namespace adsk::core;
 using namespace adsk::fusion;
@@ -42,9 +52,17 @@ Ptr<UserInterface> ui;
 const char* BUTTON_NAME = "FlatpackButton";
 const char* PANEL_TO_USE = "SolidScriptsAddinsPanel";
 const char* COMMAND_ID = "FlatpackCmdId";
+const char* FACES_INPUT = "facesSelection";
+const char* BIN_INPUT = "binSelection";
 
-
-
+template<typename T>
+Ptr<T> getSelection(Ptr<Selection> selection) {
+	Ptr<T> result;
+	if (selection->entity()->objectType() == BRepFace::classType()) {
+		result = selection->entity();
+	}
+	return result;
+}
 
 // CommandExecuted event handler.
 class OnExecuteEventHander : public adsk::core::CommandEventHandler
@@ -52,7 +70,83 @@ class OnExecuteEventHander : public adsk::core::CommandEventHandler
 public:
 	void notify(const Ptr<CommandEventArgs>& eventArgs) override
 	{
+		Ptr<Command> cmd = eventArgs->command();
 
+		if (cmd) {
+			Ptr<CommandInputs> inputs = cmd->commandInputs();
+
+			Ptr<SelectionCommandInput> selectionInput = inputs->itemById(FACES_INPUT);
+			if (selectionInput) {
+				for(size_t i=0 ; i < selectionInput->selectionCount() ; i++) {
+					Ptr<BRepFace> face = getSelection<BRepFace>(selectionInput->selection(i));
+					if (!face) {
+						ui->messageBox("Selection is not a face!");
+						return;
+					}
+
+					// Polygon
+					std::stringstream s;
+					s << "<ol>";
+					for (Ptr<BRepLoop> loop : face->loops()) {
+						// Ring
+						s << "<li>";
+						for (Ptr<BRepCoEdge> edge : loop->coEdges()) {
+							if (!edge) {
+								ui->messageBox("No edge!");
+								return;
+							}
+
+							Ptr<Curve2D> curve = edge->geometry();
+							switch (curve->curveType()) {
+							case Arc2DCurveType: 	// 	Transient 2D arc.
+								ui->messageBox("Arc2DCurveType not supported");
+								return;
+								break;
+							case Circle2DCurveType: // Transient 2D circle.
+								ui->messageBox("Circle2DCurveType not supported");
+								return;
+								break;
+							case Ellipse2DCurveType: // Transient 2D ellipse.
+								ui->messageBox("Ellipse2DCurveType not supported");
+								return;
+								break;
+							case EllipticalArc2DCurveType: // Transient 2D elliptical arc.
+								ui->messageBox("EllipticalArc2DCurveType not supported");
+								return;
+								break;
+							case InfiniteLine2DCurveType: //	Transient 2D infinite line.
+								ui->messageBox("InfiniteLine2DCurveType not supported");
+								return;
+								break;
+							case Line2DCurveType: // Transient 2D line segment.
+								ui->messageBox("Line2DCurveType not supported");
+								return;
+								break;
+							case NurbsCurve2DCurveType: // Transient 2D NURBS curve.
+								Ptr<NurbsCurve2D> nurbsCurve = curve;
+								s << "<ol>";
+								s << "<li>NurbsCurve2DCurveType: degree=" << nurbsCurve->degree() << " controlPoints=" << nurbsCurve->controlPointCount() << " knotcount=" << nurbsCurve->knotCount() << "</li>\n";
+
+
+								for (Ptr<Point2D> point : nurbsCurve->controlPoints()) {
+									//s << "(" << point->x() << ", " << point->y() << ")  ";
+								}
+								s << "</ol>";
+								break;
+
+							}
+							
+						}
+						s << "</li>";
+					}
+					s << "</ol>";
+					ui->messageBox(s.str());
+
+					
+
+				}
+			}
+		}
 	}
 };
 
@@ -86,6 +180,14 @@ public:
 				if (!isOk)
 					return;
 
+				// Connect to the execute event.
+				Ptr<CommandEvent> onExecute = command->execute();
+				if (!onExecute)
+					return;
+				isOk = onExecute->add(&onExecuteHandler);
+				if (!isOk)
+					return;
+
 				// Get the CommandInputs collection associated with the command.
 				Ptr<CommandInputs> inputs = command->commandInputs();
 				if (!inputs)
@@ -94,17 +196,17 @@ public:
 				// Create a read only textbox input.
 				//inputs->addTextBoxCommandInput("readonly_textBox", "Text Box 1", "This is an example of a read-only text box.", 2, true);
 
-				Ptr<SelectionCommandInput> facesSelectionInput = inputs->addSelectionInput("facesSelection", "Faces to export", "Faces to export");
+				Ptr<SelectionCommandInput> facesSelectionInput = inputs->addSelectionInput(FACES_INPUT, "Faces to export", "Faces to export");
 				if (!facesSelectionInput)
 					return;
 				facesSelectionInput->addSelectionFilter("PlanarFaces");
-				facesSelectionInput->setSelectionLimits(0);
+				facesSelectionInput->setSelectionLimits(1);
 
-				Ptr<SelectionCommandInput> binSelectionInput = inputs->addSelectionInput("binSelection", "Bin", "Sketch lines forming the outline of the stock material to fit the parts into");
+				Ptr<SelectionCommandInput> binSelectionInput = inputs->addSelectionInput(BIN_INPUT, "Bin", "Sketch lines forming the outline of the stock material to fit the parts into");
 				if (!binSelectionInput)
 					return;
 				binSelectionInput->addSelectionFilter("Profiles");
-				binSelectionInput->setSelectionLimits(1);
+				binSelectionInput->setSelectionLimits(1, 1);
 
 				// Create value input.
 				//inputs->addValueInput("value", "Value", "cm", ValueInput::createByReal(0.0));
